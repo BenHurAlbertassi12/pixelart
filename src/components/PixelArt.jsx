5ab3f6ed4b6665cd36eece461cac2a7e39d7f450
BenHur Albertassi da Silva <benhuralber@yahoo.com.br>
2022-12-22 20:58:42 -0300

    Thats all folks

--- src/components/PixelArt.jsx
+++ src/components/PixelArt.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 // import { useRef, createElement } from 'react';
 
-import '../css/pixelartpaleta.css';
+import '../css/pixelArtPaleta.css';
 
 
 function PixelArt() {
@@ -24,26 +24,26 @@ function PixelArt() {
   // corDoFundo[14].style.backgroundColor = '#8080ff';
   // corDoFundo[15].style.backgroundColor = 'darkgrey';
 
-  function quadro25x25() {
-      for (let index = 0; index < 25; index+1) {
-        let divPixel = React.createElement('div', {
-          className: 'pixel',
-        },
-        );
-          return divPixel
-    }
-      };
+  const quadro25x25 = () => {
+     for (let index = 0; index < 25; index += 1) {
+       const divPixel = React.createElement('div', {
+         className: 'pixel',
+       });
+       return divPixel;
+     }
+  };
     
-
+console.log('teste', quadro25x25());
     
   return (
     <div>
       <div id='pixel-board'>
-      {quadro25x25()}
-      </div>;
+        {quadro25x25()}
+      </div>
+      ;
     </div>
   );
     
 
 }
-export default PixelArt
+export default PixelArt;
